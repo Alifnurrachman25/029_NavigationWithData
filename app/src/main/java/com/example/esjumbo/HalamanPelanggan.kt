289c2746d9632f3c29d76d68fae357a992310532
Alif Nur Rachman <alif.nur.ft21@mail.umy.ac.id>
2023-11-14 06:58:37 +0700

    Menambahkan sedikit arangement dan background untuk tampilan halaman pelanggan

--- app/src/main/java/com/example/esjumbo/HalamanPelanggan.kt
+++ app/src/main/java/com/example/esjumbo/HalamanPelanggan.kt
@@ -1,6 +1,10 @@
 package com.example.esjumbo
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -10,6 +14,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -28,7 +35,13 @@ fun HalamanPelanggan() {
         mutableStateOf("")
     }
 
-    Column {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
         OutlinedTextField(
             value = nama,
             onValueChange = {},
@@ -44,6 +57,7 @@ fun HalamanPelanggan() {
             onValueChange = {},
             label = { Text(text = "Alamat")}
         )
+
 
     }
 }
