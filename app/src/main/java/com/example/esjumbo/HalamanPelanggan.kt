76f39f50908faec3e9bbc266e61feef338d692e7
Alif Nur Rachman <alif.nur.ft21@mail.umy.ac.id>
2023-11-14 06:53:22 +0700

    Menambahkan textfield nomor telpon

--- app/src/main/java/com/example/esjumbo/HalamanPelanggan.kt
+++ app/src/main/java/com/example/esjumbo/HalamanPelanggan.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,11 +19,20 @@ fun HalamanPelanggan() {
     var nama by rememberSaveable {
         mutableStateOf("")
     }
+
+    var noHP by remember {
+        mutableStateOf("")
+    }
     Column {
         OutlinedTextField(
             value = nama,
             onValueChange = {},
             label = { Text(text = "Nama")}
+        )
+        OutlinedTextField(
+            value = noHP,
+            onValueChange = {},
+            label = { Text(text = "Nomor telpon")}
         )
     }
 }
